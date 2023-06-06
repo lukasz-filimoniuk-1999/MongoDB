342e01d3b58ddfa863584caf58c63124fab116eb
KrzysztofKapliński <treacheuw@gmail.com>
2023-06-06 16:02:41 +0200

    Dodanie połączenia z bazą MongoDb i pliku konfiguracyjnego.

--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MongoDB
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.FileProviders;
+
+namespace DBClient
 {
     class Program
     {
+        static IConfigurationRoot configuration;
+        static IMongoDatabase db;
+
         static void Main(string[] args)
         {
+            SetConfig();
+            SetDbConnection();
+
+            var collection = db.GetCollection<BsonDocument>("Rating");
+            var filter = Builders<BsonDocument>.Filter.Empty;
+            var documents = collection.Find(filter).ToList();
+            Console.WriteLine(documents);
+        }
+
+        static void SetConfig()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("config\\settings.json", optional: true, reloadOnChange: true)
+                .Build();
+        }
+
+        static void SetDbConnection()
+        {
+            var client = new MongoClient(configuration["db:connectionString"]);
+            db = client.GetDatabase(configuration["db:dbName"]);
         }
     }
 }
